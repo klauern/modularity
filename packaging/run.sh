7ab6bb5347aad08cea228424bf5c4062791d4a14
Nick Klauer <klauer@gmail.com>
2016-06-28 20:15:21 -0500

    add steps for packaging

---
+++ packaging/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p mlib
+
+jar --create --file=mlib/org.astro@1.0.jar \
+            --module-version=1.0 -C ../astro-greetings/mods/org.astro .
+
+jar --create --file=mlib/com.greetings.jar \
+            --main-class=com.greetings.Main -C ../astro-greetings/mods/com.greetings .
+
+ls mlib
+
+java -mp mlib -m com.greetings
+
+jar --print-module-descriptor --file=mlib/org.astro@1.0.jar
