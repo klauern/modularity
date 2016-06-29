6373b70200c4d53ed505c68eaf890f04e70a9a54
Nick Klauer <klauer@gmail.com>
2016-06-28 20:18:08 -0500

    move things around and update run.sh for astro-greetings

---
+++ astro-greetings/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir -p mods/{org.astro,com.greetings}
+
+javac -d mods/org.astro \
+            src/org.astro/module-info.java src/org.astro/org/astro/World.java
+
+javac -modulepath mods -d mods/com.greetings \
+            src/com.greetings/module-info.java src/com.greetings/com/greetings/Main.java
+
+java -modulepath mods -m com.greetings/com.greetings.Main
+
