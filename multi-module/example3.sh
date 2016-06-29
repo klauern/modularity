dee577ca8d21d2ee25aaa8e003a7c76e25f45d21
Nick Klauer <klauer@gmail.com>
2016-06-29 13:57:29 -0500

    refactor multi-module run.sh

--- multi-module/example3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p multi-module/mods
-
-javac -d multi-module/mods -modulesourcepath astro-greetings/src $(find astro-greetings/src -name '*.java')
-
-find multi-module/mods -type f
---
+++ multi-module/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -p mods
+
+javac -d mods -modulesourcepath ../astro-greetings/src $(find ../astro-greetings/src -name '*.java')
+
+find mods -type f
